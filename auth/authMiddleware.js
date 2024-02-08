d9d2e7b88335c3852fa2a1472baaa21b6c749289
Łukasz Leśniak <lukaszlesniaksw@gmail.com>
2024-02-09 00:01:32 +0100

    create jwt strategy and auth middleware

---
+++ auth/authMiddleware.js
@@ -0,0 +1,17 @@
+import passport from "passport";
+
+const auth = (req, res, next) => {
+  passport.authenticate("jwt", { session: false }, (err, user) => {
+    if (!user || err) {
+      return res.status(401).json({
+        status: "error",
+        code: 401,
+        message: "Not authorized",
+      });
+    }
+    req.user = user;
+    next();
+  })(req, res, next);
+};
+
+export { auth };
