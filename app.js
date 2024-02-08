a2dc40f95af9d450fe951ef1c043f75ce2919200
Łukasz Leśniak <lukaszlesniaksw@gmail.com>
2024-02-09 00:02:18 +0100

    import jwt strategy and add new routes

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import "./auth/strategyJWT.js";
 
 import { router as contactsRouter } from "./routes/api/contacts.js";
+import { router as usersRouter } from "./routes/api/users.js";
 
 const app = express();
 
@@ -13,6 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
+app.use("/api/users", usersRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
